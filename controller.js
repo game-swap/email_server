7e1d8777b1274d2b5fd81864a8fec6e46a11eaa6
Adam Reback <adamreback@Adams-MacBook-Pro.local>
2019-05-08 18:57:09 -0700

    Reverted to bcc method of distribution

--- controller.js
+++ controller.js
@@ -10,10 +10,11 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-const makeMessage = (email, game, offer, contact) => {
+const makeMessage = (emails, game, offer, contact) => {
   var mailOptions = {
     from: 'the.game.swap.app@gmail.com',
-    to: email,
+    to: '',
+    bcc: emails,
     subject: 'Sorry, I need to test this',
     text: 'I don\'t mean to spam you, I just need to see if this method works.\nSorry'
   };
@@ -26,18 +27,24 @@ const controller = {
     post: (req, res) => {
       /*TODO query db for users who have posted
         and structure their info to send templated emails
+
+        expected req.body = {
+          userID: number,
+          gameID: number,
+          offer: ['string'],
+          console: string
+        }
       */
-    
-    for (var x = 0; x < list.length; x++) {
-      transporter.sendMail(makeMessage(list[x]), function(error, info){
+
+      //list is temporary, will be replaced with the list of emails to have messages sent to them
+      transporter.sendMail(makeMessage(list), function(error, info) {
         if (error) {
           res.status(404).send(error);
-          return;
+        } else {
+          res.status(201).send('Emails sent');
         }
       });
     }
-    res.status(201).send('Emails sent');
-  }
 }
 
 module.exports = controller;
