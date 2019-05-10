9113317438ad1d4847d3fa3136e7ef8cc3d711e2
Adam Reback <adamreback@Adams-MacBook-Pro.local>
2019-05-10 10:12:50 -0700

    Post request now updates user's number of requests before sending email

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@ const db = require('./db.js');
 const nodemailer = require('nodemailer');
 const { pass } = require('./config.js');
 
+//config for nodemailer to send email
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -10,55 +11,78 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-const makeMessage = (emails, game, offer, contact) => {
+//creates email template
+const makeMessage = (emails, buyer, gameName, offer, contact) => {
+  var offerStr = '\n';
+  for (var y = 0; y < offer.length; y++) {
+    offerStr += offer[y] + '\n'
+  }
   var mailOptions = {
     from: 'the.game.swap.app@gmail.com',
     to: '',
     bcc: emails,
-    subject: 'Sorry, I need to test this',
-    text: 'I don\'t mean to spam you, I just need to see if this method works.\nSorry'
+    subject: 'Trade Offer',
+    text: 
+      `Good news!\n\
+      \nThere is someone interested in trading for your copy of ${gameName}! ${buyer} is offering: \n${offerStr}\nIf you're interested in this deal, contact ${buyer} at ${contact}.\n\nKeep swappin'\nGame Swap\
+      `
   };
   return mailOptions;
 }
 
 const controller = {
+
+    //route to send email to all sellers
     post: (req, res) => {
-      /*TODO query db for users who have posted
-        and structure their info to send templated emails
 
-        expected req.body = {
-          userID: number,
-          gameID: number,
-          offer: ['string'],
-          console: string
-        }
-      */
+      let { buyer, user_id, game, offer, email, game_id, platform_id } = req.body;
 
-        db.query(`\
-          SELECT email FROM users u\
-          INNER JOIN offers o\
-          ON u.user_id = o.user_id\
-          INNER JOIN games g\
-          ON o.game_id = g.game_id\
-          WHERE g.game_id = ${req.body.game_id}\
-          AND o.platform_id = ${req.body.platform_id}\
+      //Updates user's number of requests left
+      db
+        .query(`\
+                SELECT requests_left FROM users
+                WHERE user_id = ${user_id}
+                `)
+        .then(data => {
+          var updatedVal = data.rows[0].requests_left - 1;
+          db
+          .query(`UPDATE users
+          SET requests_left = ${updatedVal}
+          WHERE user_id = ${user_id}
           `)
           .then(data => {
-            var emails = [];
-            for (var x = 0; x < data.rows.length; x++) {
-              emails.push(data.rows[x].email)
-            }
-            transporter.sendMail(makeMessage(emails), function(error, info) {
-              if (error) {
-                res.status(404).send(error);
-              } else {
-                //TODO decrease user's number of emails remaining
-                res.status(201).send('Emails sent');
-              }
-            });
-          })
-          .catch(err => res.status(404).send(err))
-        }
+              //queryies database for all sellers' emails
+              db.query(`
+                SELECT email FROM users u
+                INNER JOIN offers o
+                ON u.user_id = o.user_id
+                INNER JOIN games g
+                ON o.game_id = g.game_id
+                WHERE g.game_id = ${game_id}
+                AND o.platform_id = ${platform_id}
+                `)
+                .then(data => {
+                  var emails = [];
+                  for (var x = 0; x < data.rows.length; x++) {
+                    emails.push(data.rows[x].email)
+                  }
+                  //sends email
+                  transporter.sendMail(makeMessage(emails, buyer, game, offer, email), function(error, info) {
+                    if (error) {
+                      res.status(404).send(error);
+                    } else {
+                      res.status(201).send('Emails sent')
+                    }
+                  });
+                })
+                .catch(err => res.status(404).send(err))
+            })
+            .catch(err => res.status(404).send(err))
+        })
+        .catch(err => res.status(404).send(err))
+
+
       }
+    }
 
 module.exports = controller;
